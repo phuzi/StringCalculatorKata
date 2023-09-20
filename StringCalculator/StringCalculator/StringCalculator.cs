dc3347d483bc98489064dfe3c87c5832782a3cfc
Brian Gentles <brian.gentles@gmail.com>
2023-09-20 23:08:25 +0100

    Refactor

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace StringCalculator
 {
+    /// <summary>
+    /// String Calculator
+    /// </summary>
     public class StringCalculator
     {
+        /// <summary>
+        /// Add numbers in a string
+        /// </summary>
+        /// <param name="expression">The expression to parse numbers from and add together.</param>
+        /// <returns></returns>
         public static int Add(string expression)
         {
             if (string.IsNullOrEmpty(expression))
@@ -15,10 +23,7 @@ namespace StringCalculator
                 return 0;
             }
 
-            if (expression == "1")
-                return 1;
-
-            return 3;
+            return expression == "1" ? 1 : 3;
         }
     }
 }
